7b99634771dd5d3cfdc9a69e88e618017c3ff38a
Claudia <serbe_03@hotmail.com>
2020-02-09 18:55:27 -0300

    Tp: FerreteFacturacion - FerreteConstruccion.

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -4,15 +4,43 @@ B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un ter
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
 
+var largoTerreno;
+var anchoTerreno;
+var radioTerreno;
+var cantidadAlambre;
+var mensaje = "El alambre necesario es: ";
+
+function ObtenerMedidas(){
+    largoTerreno = parseInt(document.getElementById("Largo").value);
+    anchoTerreno = parseInt(document.getElementById("Ancho").value);
+    radioTerreno = parseInt(document.getElementById("Radio").value);
+}
+
 function Rectangulo () 
 {
-
+    ObtenerMedidas();
+    cantidadAlambre = (2 *largoTerreno + 2* anchoTerreno) * 3;
+    alert(mensaje + cantidadAlambre);
 }
 function Circulo () 
 {
-	
+	ObtenerMedidas();
+    cantidadAlambre = 2 * radioTerreno * 3.14;
+    alert(mensaje + cantidadAlambre);
 }
 function Materiales () 
 {
-	
+    /*Aca depende de si es rectangulo o circulo, hallar el area. Por metro cuadrado, la relacion es 2 bolsas de cemento y 3 de cal.
+    */
+    var areaRectangulo = largoTerreno * anchoTerreno;
+    var areaCirculo = radioTerreno * radioTerreno * 3.14;
+    
+    var cementoRectangulo = areaRectangulo * 2;
+    var calRectangulo = areaRectangulo * 3;
+
+    var cementoCirculo = areaCirculo * 2;
+    var calCirculo = areaCirculo * 3;
+
+    alert("Para el rectangulo, se necesitan "+cementoRectangulo+" cementos y "+calRectangulo+" de cal.");
+    alert("Para el circulo, se necesitan "+cementoCirculo+" cementos y "+calCirculo+" de cal.");
 }
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -3,15 +3,34 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
+
+var precioUno;
+var precioDos;
+var precioTres;
+var resultado;
+var mensaje = "El resultado es: ";
+
+function ObtenerPrecios(){
+    precioUno = parseInt(document.getElementById("PrecioUno").value);
+    precioDos = parseInt(document.getElementById("PrecioDos").value);
+    precioTres = parseInt(document.getElementById("PrecioTres").value);
+}
+
 function Sumar () 
 {
-	
+    ObtenerPrecios();
+    resultado = precioUno + precioDos + precioTres;
+    alert(mensaje + resultado);
 }
 function Promedio () 
 {
-	
+    ObtenerPrecios();
+    resultado = resultado / 3;
+    alert(mensaje + resultado);
 }
 function PrecioFinal () 
 {
-	
+    ObtenerPrecios();
+    resultado = (resultado * 21 / 100) + resultado;
+    alert(mensaje + resultado);
 }
