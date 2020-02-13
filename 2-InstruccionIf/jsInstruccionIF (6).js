2bb44af10aa50839e805ce14d037754685ba7ef5
alumno <alumno@utnfra.com.ar>
2020-02-12 21:54:01 -0300

    cj: commit furioso

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -4,7 +4,9 @@ function mostrar() {
 
   if (edad >= 18) {
     alert("Es mayor de edad.");
-  } else if (edad <= 17 && edad >= 13) {
+  } else if (edad >= 13) {
+    // (edad <= 17 se saca esta condicion, por que de arriba ya entrarian las que son
+    //menores a 18, hay que ver no mas las que son mayores a 13)
     alert("Es adolescente.");
   } else {
     alert("Es menor de edad.");
