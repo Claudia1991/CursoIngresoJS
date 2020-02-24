04b433a22c140f53e638803c91ce4feb8176c0bd
Claudia <serbe_03@hotmail.com>
2020-02-24 11:48:33 -0300

    Ejercio examen

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,104 @@
-function mostrar()
-{
+function mostrar() {
+    /**
+     * Realizar el algoritmo que permita iterar el ingreso de dos datos, una letra y un número entre -100 y 100 (validar) 
+     * hasta que el usuario quiera e informar al terminar el ingreso por document.write:
+    a) La cantidad de números pares.
+    b) La cantidad de números impares.
+    c) La cantidad de ceros.
+    d) El promedio de todos los números positivos ingresados.
+    e) La suma de todos los números negativos.
+    f) El número y la letra del máximo y el mínimo.
+     */
+    //Mensajes
+    var mensajeCantidadNumerosPares = "La cantidad de números pares: ";
+    var mensajeCantidadNumerosImpares = "La cantidad de números impares: ";
+    var mensajeCantidadNumerosCeros = "La cantidad de números ceros: ";
+    var mensajePromedioNumerosPositivos = "El promedio de números positivos: ";
+    var mensajeSumaNumerosNegativos = "La suma de los numeros negativos: ";
+    var mensajeNumeroLetraMinimo = "El numero y letra minimo es: ";
+    var mensajeNumeroLetraMaximo = "El numero y letra maximo es: ";
+    //Declaracion de variables
+    var contadorIngresos = 0;
+    var numeroIngresado;
+    var letraIngresada;
+    var cantidadNumerosPares = 0;
+    var cantidadNumerosImpares = 0;
+    var cantidadDeCeros = 0;
+    var sumaNumerosPositivos;
+    var cantidadNumerosPositivos;
+    var sumaNumerosNegativos;
+    var numeroMinimo;
+    var letraMinimo;
+    var letraMaximo;
+    var numeroMaximo;
+    var respuesta = "si";
+    while (!isValidAnwser(respuesta) && respuesta != "no") {
+        //Ingreso de datos
+        numeroIngresado = prompt("Ingrese un numero entre -100 y 100: ");
+        while (!isValidNumber(numeroIngresado)) {
+            numeroIngresado = prompt("Ingrese un numero entre -100 y 100: ");
+        }
+        letraIngresada = prompt("Ingrese una letra: ");
 
+        //Parseo de numero
+        numeroIngresado = parseInt(numeroIngresado);
+
+        //Logica minimo - maximo
+        if (contadorIngresos === 0) {
+            //Es el primer ingreso de datos
+            numeroMinimo = numeroIngresado;
+            letraMinimo = letraIngresada;
+            contadorIngresos++;
+        } else {
+            //No es el primer ingreso de datos, se actualizan minimo - maximo segun corrresponda.
+            if (numeroMinimo > numeroIngresado) {
+                numeroMinimo = numeroIngresado;
+                letraMinimo = letraIngresada;
+            }
+            if (numeroMaximo < numeroIngresado) {
+                numeroMaximo = numeroIngresado;
+                letraMaximo = letraIngresada;
+            }
+        }
+
+        //Logico numeros positivos, negativos, ceros
+        if (numeroIngresado > 0) {
+            sumaNumerosPositivos += numeroIngresado;
+            cantidadNumerosPositivos++;
+        } else if (numeroIngresado < 0) {
+            sumaNumerosNegativos += numeroIngresado;
+        } else {
+            cantidadDeCeros++;
+        }
+
+        //Logica numeros pares, impares
+        if (numeroIngresado % 2 === 0) {
+            cantidadNumerosPares++;
+        } else {
+            cantidadNumerosImpares++;
+        }
+        //Pregunto al usuario si quiere seguir ingresando datos.
+        respuesta = prompt("Quiere seguir ingresando? (para terminar ingreso 'no'): ");
+    }
+
+    //Imprimo los datos por pantalla.
+    mensajeCantidadNumerosPares = mensajeCantidadNumerosPares.concat(cantidadNumerosPares, "<br>");
+    mensajeCantidadNumerosImpares = mensajeCantidadNumerosImpares.concat(cantidadNumerosImpares, "<br>");
+    mensajeCantidadNumerosCeros = mensajeCantidadNumerosCeros.concat(cantidadDeCeros, "<br>");
+    mensajePromedioNumerosPositivos = mensajePromedioNumerosPositivos.concat((sumaNumerosPositivos / cantidadNumerosPositivos), "<br>");
+    mensajeSumaNumerosNegativos = mensajeSumaNumerosNegativos.concat(sumaNumerosNegativos, "<br>");
+    mensajeNumeroLetraMinimo = mensajeNumeroLetraMinimo.concat(numeroMinimo, " ", letraMinimo, " ", "<br>");
+    mensajeNumeroLetraMaximo = mensajeNumeroLetraMaximo.concat(numeroMaximo, " ", letraMaximo, " ", "<br>");
+
+    document.write(mensajeCantidadNumerosPares + mensajeCantidadNumerosImpares + mensajeCantidadNumerosCeros + mensajePromedioNumerosPositivos + mensajeSumaNumerosNegativos + mensajeNumeroLetraMinimo + mensajeNumeroLetraMaximo);
+}
+
+function isValidNumber(number) {
+    //Valido que el numero este entre -100 y 100
+    return (!isNaN(number) && ((parseInt(number) >= -100) && (parseInt(number) <= 100)));
+}
+
+function isValidAnwser(anwser) {
+    //valido que la respuesta sea no,n, N, NO
+    return (isNaN(anwser) && (anwser.toLowerCase() === "no" || anwser.toLowerCase() === "n"));
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,8 +1,69 @@
 function mostrar() {
-    /**
-     * Realizar el algoritmo que permita el ingreso por prompt de las notas (validar entre 0 y 10) , el sexo (validar el sexo “f” o “m”) de 5 alumnos, informar por alert:
-a) El promedio de las notas totales.
-b) La nota más baja y el sexo de esa persona.
-c) La cantidad de varones que su nota haya sido mayor o igual a 6.
-     */
+     /**
+      * Realizar el algoritmo que permita el ingreso por prompt de las notas (validar entre 0 y 10) , 
+      * el sexo (validar el sexo “f” o “m”) de 5 alumnos, informar por alert:
+      a) El promedio de las notas totales.
+      b) La nota más baja y el sexo de esa persona.
+      c) La cantidad de varones que su nota haya sido mayor o igual a 6.
+      */
+     //Declaracion de variables.
+     var notas = 0;
+     var notaMasBaja = 0;
+     var sexoNotaMasBaja;
+     var cantidadVaronesNota = 0;
+     var cantidadNotasAIngresar = 5;
+     var notaIngresada;
+     var sexoIngresado;
+
+     //Ingreso de datos
+     for (var i = 0; i < cantidadNotasAIngresar; i++) {
+          //Ingreso de nota
+          notaIngresada = prompt("Ingrese la nota del alumno: ");
+          while (!IsValidNote(notaIngresada)) {
+               //Nota no valida
+               notaIngresada = prompt("Ingrese la nota del alumno: ");
+          }
+          //Ingreso de sexo
+          sexoIngresado = prompt("Ingrese el sexo del alumno: ");
+          while (!IsValidSex(sexoIngresado)) {
+               //Sexo no valido
+               sexoIngresado = prompt("Ingrese el sexo del alumno: ");
+          }
+          //Parseo de string a number
+          notaIngresada = parseInt(notaIngresada);
+          
+          //Acumulo las notas.
+          notas = notas + notaIngresada;
+
+          //Busco la nota mas baja
+          if (i === 0) {
+               notaMasBaja = notaIngresada;
+               sexoNotaMasBaja = sexoIngresado;
+          } else {
+               if (notaMasBaja > notaIngresada) {
+                    notaMasBaja = notaIngresada;
+                    sexoNotaMasBaja = sexoIngresado;
+               }
+          }
+
+          //La cantidad de varones que su nota haya sido mayor o igual a 6.
+          if (sexoIngresado === "m" && notaIngresada >= 6) {
+               cantidadVaronesNota++;
+          }
+     }
+
+     //Mostrar los datos por alert
+     alert("Promedio de notas: " + (notas / cantidadNotasAIngresar));
+     alert("La nota más baja: " + notaMasBaja + " y el sexo de esa persona: " + sexoNotaMasBaja);
+     alert("La cantidad de varones que su nota haya sido mayor o igual a 6: " + cantidadVaronesNota);
+}
+
+function IsValidSex(sex) {
+     //Validamos si lo ingresado por el usuario es f o m.
+     return ((isNaN(sex)) && ((sex.toLowerCase() === "f") || (sex.toLowerCase() === "m")));
+}
+
+function IsValidNote(note) {
+     //Validamos si lo ingresado por el usuario es una nota valida[0-10]
+     return (!isNaN(note) && (parseInt(note)>= 0) && (parseInt(note)<= 10));
 }
